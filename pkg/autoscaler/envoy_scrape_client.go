4019a4c8f25822bd5fa7c7559bd58567488396fd
Daishan <daishan@rancher.com>
2019-09-12 14:43:32 -0700

    Add linkerd proxy scrape

--- pkg/autoscaler/envoy_scrape_client.go
+++ pkg/autoscaler/envoy_scrape_client.go
@@ -15,27 +15,29 @@ import (
 	corev1lister "k8s.io/client-go/listers/core/v1"
 )
 
-type envoyScrapeClient struct {
+type proxyScrapeClient struct {
 	httpClient         *http.Client
 	metric             *Metric
 	podLister          corev1lister.PodLister
 	lastScrapedRequest map[string]int64
+	proxyType          string
 }
 
-func newEnvoyScrapeClient(httpClient *http.Client, metric *Metric, podLister corev1lister.PodLister) (*envoyScrapeClient, error) {
+func newProxyScrapeClient(httpClient *http.Client, metric *Metric, podLister corev1lister.PodLister, proxyType string) (*proxyScrapeClient, error) {
 	if httpClient == nil {
 		return nil, errors.New("HTTP client must not be nil")
 	}
 
-	return &envoyScrapeClient{
+	return &proxyScrapeClient{
 		httpClient:         httpClient,
 		metric:             metric,
 		podLister:          podLister,
 		lastScrapedRequest: map[string]int64{},
+		proxyType:          proxyType,
 	}, nil
 }
 
-func (c *envoyScrapeClient) Scrape(url string) (*Stat, error) {
+func (c *proxyScrapeClient) Scrape(url string) (*Stat, error) {
 	r1, err := labels.NewRequirement("app", selection.Equals, []string{c.metric.Labels["app"]})
 	if err != nil {
 		return nil, err
@@ -49,25 +51,35 @@ func (c *envoyScrapeClient) Scrape(url string) (*Stat, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	var activeRequest, totalRequest, podCount int64
+	now := time.Now()
+	metricKey := fmt.Sprintf("%s/%s", c.metric.Namespace, c.metric.Name)
+	stats := &Stat{
+		Time:    &now,
+		PodName: scraperPodName,
+	}
+	pod := &corev1.Pod{}
+	podMap := map[string]*corev1.Pod{}
 	for _, p := range pods {
-		if p.Status.Phase != corev1.PodRunning {
-			continue
+		if p.Status.Phase == corev1.PodRunning {
+			podMap[p.Name] = p
 		}
-		scrapeURL := fmt.Sprintf("http://%s:15090/stats/prometheus", p.Status.PodIP)
-		req, err := http.NewRequest(http.MethodGet, scrapeURL, nil)
-		if err != nil {
-			return nil, err
-		}
-		resp, err := c.httpClient.Do(req)
-		if err != nil {
-			continue
-		}
-		defer resp.Body.Close()
-		scannner := bufio.NewScanner(resp.Body)
+	}
+	podCount = int64(len(podMap))
+	for _, p := range podMap {
+		pod = p
+	}
+	if pod == nil {
+	    return stats, nil
+	}
+
+	var metricURL, rqMatchCriteria, acrqMatchCriteria string
+	if c.proxyType == "envoy" {
+		metricURL = fmt.Sprintf("http://%s:15090/stats/prometheus", pod.Status.PodIP)
 		port := c.metric.Labels["container-port"]
 		if port == "" {
-			for _, con := range p.Spec.Containers {
+			for _, con := range pod.Spec.Containers {
 				if con.Name == c.metric.Name {
 					if len(con.Ports) > 0 {
 						port = strconv.Itoa(int(con.Ports[0].ContainerPort))
@@ -79,34 +91,43 @@ func (c *envoyScrapeClient) Scrape(url string) (*Stat, error) {
 			port = "80"
 		}
 
-		rqMatchCriteria := fmt.Sprintf("envoy_http_downstream_rq_total{http_conn_manager_prefix=\"%s_%s\"}", p.Status.PodIP, port)
-		acrqMatchCriteria := fmt.Sprintf("envoy_http_downstream_rq_active{http_conn_manager_prefix=\"%s_%s\"}", p.Status.PodIP, port)
-		for scannner.Scan() {
-			if strings.Contains(scannner.Text(), rqMatchCriteria) {
-				parts := strings.Split(scannner.Text(), " ")
-				if len(parts) == 2 {
-					rqs, _ := strconv.Atoi(parts[1])
-					totalRequest += int64(rqs)
-				}
-			}
-			if strings.Contains(scannner.Text(), acrqMatchCriteria) {
-				parts := strings.Split(scannner.Text(), " ")
-				if len(parts) == 2 {
-					rqs, _ := strconv.Atoi(parts[1])
-					activeRequest += int64(rqs)
-				}
+		rqMatchCriteria = fmt.Sprintf("envoy_http_downstream_rq_total{http_conn_manager_prefix=\"%s_%s\"}", pod.Status.PodIP, port)
+		acrqMatchCriteria = fmt.Sprintf("envoy_http_downstream_rq_active{http_conn_manager_prefix=\"%s_%s\"}", pod.Status.PodIP, port)
+	} else if c.proxyType == "linkerd" {
+		metricURL = fmt.Sprintf("http://%s:4191/metrics", pod.Status.PodIP)
+		rqMatchCriteria = "request_handle_us_count{direction=\"inbound\"}"
+		acrqMatchCriteria = "tcp_open_connections{direction=\"inbound\",peer=\"src\",tls=\"no_identity\",no_tls_reason=\"not_provided_by_remote\"}"
+	}
+
+	req, err := http.NewRequest(http.MethodGet, metricURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return stats, nil
+	}
+	defer resp.Body.Close()
+	scannner := bufio.NewScanner(resp.Body)
+
+	for scannner.Scan() {
+		if strings.Contains(scannner.Text(), rqMatchCriteria) {
+			parts := strings.Split(scannner.Text(), " ")
+			if len(parts) == 2 {
+				rqs, _ := strconv.Atoi(parts[1])
+				totalRequest = int64(rqs)
 			}
 		}
-		podCount++
+		if strings.Contains(scannner.Text(), acrqMatchCriteria) {
+			parts := strings.Split(scannner.Text(), " ")
+			if len(parts) == 2 {
+				rqs, _ := strconv.Atoi(parts[1])
+				activeRequest = int64(rqs)
+			}
+		}
 	}
-	now := time.Now()
-	metricKey := fmt.Sprintf("%s/%s", c.metric.Namespace, c.metric.Name)
-	stats := &Stat{
-		Time:                      &now,
-		PodName:                   scraperPodName,
-		AverageConcurrentRequests: float64(activeRequest),
-		RequestCount:              int32(totalRequest - c.lastScrapedRequest[metricKey]),
-	}
-	c.lastScrapedRequest[metricKey] = totalRequest
+	stats.AverageConcurrentRequests = float64(activeRequest*podCount)
+	stats.RequestCount = int32(totalRequest*podCount - c.lastScrapedRequest[metricKey])
+	c.lastScrapedRequest[metricKey] = totalRequest*podCount
 	return stats, nil
 }
